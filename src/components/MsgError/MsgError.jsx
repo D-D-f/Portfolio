07b084323c5a8a782c1fb6533aa010ae9b2a5567
D-D-F <defreitas-david@outlook.fr>
2023-05-22 15:44:06 +0200

    modification du terminal

---
+++ src/components/MsgError/MsgError.jsx
@@ -0,0 +1,10 @@
+const MsgError = () => {
+  return (
+    <p>
+      La commande que vous avez taper n'existe pas. Taper help pour connaître
+      l'ensemble des commandes.
+    </p>
+  );
+};
+
+export default MsgError;
--- src/components/Terminal/Terminal.jsx
+++ src/components/Terminal/Terminal.jsx
@@ -4,10 +4,12 @@ import classes from "./Terminal.module.css";
 import Fields from "../Fields/Fields";
 import Presentation from "../Presentation/Presentation";
 import Notification from "../Notification/Notification";
+import MsgError from "../MsgError/MsgError";
 import Help from "../Help/Help";
 
 const Terminal = () => {
   const [history, setHistory] = useState([]);
+
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       if (
@@ -19,9 +21,38 @@ const Terminal = () => {
           ...current,
           { id: uuid(), value: event.target.value },
         ]);
+      } else {
+        event.target.disabled = true;
+        setHistory((current) => [
+          ...current,
+          {
+            id: uuid(),
+            value:
+              "La commande n'existe pas. Taper help pour connaitre l'ensemble des commandes",
+          },
+        ]);
       }
     }
   };
+
+  const commandTerminal = (command, id) => {
+    if (command === "help") {
+      return <Help key={id} />;
+    } else if (command === "presentation") {
+      return <Presentation key={id} />;
+    } else {
+      return <MsgError key={id} />;
+    }
+  };
+
+  const displayCommand = history.map((item, i) => {
+    return (
+      <React.Fragment key={i}>
+        {commandTerminal(item.value, item.id)}
+        <Fields handleKeyPress={handleKeyPress} />
+      </React.Fragment>
+    );
+  });
 
   return (
     <div className={classes.terminal}>
@@ -36,18 +67,7 @@ const Terminal = () => {
       </ul>
       <div className={classes.container_terminal}>
         <Fields handleKeyPress={handleKeyPress} />
-        {history.map((item, i) => {
-          return (
-            <React.Fragment key={item.id}>
-              {item.value === "help" ? (
-                <Help key={item.id} />
-              ) : (
-                <Presentation key={item.id} />
-              )}
-              <Fields key={`champ_${i}`} handleKeyPress={handleKeyPress} />
-            </React.Fragment>
-          );
-        })}
+        {displayCommand}
       </div>
       <div className={classes.notification}>
         <Notification message="Pour connaître les commandes du terminal tapez help puis appuyer sur la touche entrer" />
